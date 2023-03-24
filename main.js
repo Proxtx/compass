d83f93d2d7eba6466dc85a66b5c47f570fd371e6
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-24 17:45:18 +0100

    commit since working

---
+++ main.js
@@ -0,0 +1,232 @@
+const routes = {
+  1: {
+    0: {
+      //spielplatz
+      code: 825,
+      hint: "Jetzt steht ein kleiner Parkourlauf an! Schnappt euch ein Bier, rutscht die Rutsche runter, lauft geradeaus auf das Kletter-/Seilgerüst zu und klettert einmal im Kreis. Dabei muss bis zum Ende ein Bier getrunken werden! (Bitte filmen und in die Gruppe schicken; Bier muss im Video geöffnet werden und am Ende nachgewiesen leer sein/vom Läufer getrunken)",
+      location: [50.9933246, 7.1046091],
+    },
+    825: {
+      //kita
+      code: 216,
+      hint: "Ob ihr zählen und rechnen könnt? Wieviele Buchstaben sind auf dem Klingelschild der Kindertagesstätte, multipliziert mit dem morgigen Alter der Gastgeberin?",
+      location: [50.9971046, 7.1007708],
+    },
+    216: {
+      //hansen
+      code: 492,
+      hint: "Wat fott es, es fott! Kauft ein Kölsch im Wirtshaus Hansen, ext es und der Barkeeper sagt euch die nächste Nummer!",
+      location: [50.998862, 7.0999595],
+    },
+    492: {
+      //hochhäuser
+      code: 88416,
+      hint: "Hoch hinaus! Die Hochhäuser haben einen kleinen Park mit 9 Bäumen, addiert die Nummern die auf ihnen stehen, das ergibt die nächste Zahl!",
+      location: [50.99922255022839, 7.1245154852040455],
+      _location: [50.9914574, 7.096766],
+    },
+    88416: {
+      //Finja home
+      code: 13,
+      hint: "Hinter Gittern! Haut als Team soviel Bier weg wie es streben auf Finjas Toilette (von außen) gibt!",
+      location: [50.9958535, 7.1011889],
+    },
+  },
+
+  2: {
+    88416: {
+      //spielplatz
+      code: 825,
+      hint: "Jetzt steht ein kleiner Parkourlauf an! Schnappt euch ein Bier, rutscht die Rutsche runter, lauft geradeaus auf das Kletter-/Seilgerüst zu und klettert einmal im Kreis. Dabei muss bis zum Ende ein Bier getrunken werden! (Bitte filmen und in die Gruppe schicken; Bier muss im Video geöffnet werden und am Ende nachgewiesen leer sein/vom Läufer getrunken)",
+      location: [50.9933246, 7.1046091],
+    },
+    0: {
+      //kita
+      code: 216,
+      hint: "Ob ihr zählen und rechnen könnt? Wieviele Buchstaben sind auf dem Klingelschild der Kindertagesstätte, multipliziert mit dem morgigen Alter der Gastgeberin?",
+      location: [50.9971046, 7.1007708],
+    },
+    216: {
+      //hansen
+      code: 492,
+      hint: "Wat fott es, es fott! Kauft ein Kölsch im Wirtshaus Hansen, ext es und der Barkeeper sagt euch die nächste Nummer!",
+      location: [50.998862, 7.0999595],
+    },
+    492: {
+      //hochhäuser
+      code: 88416,
+      hint: "Hoch hinaus! Die Hochhäuser haben einen kleinen Park mit 9 Bäumen, addiert die Nummern die auf ihnen stehen, das ergibt die nächste Zahl!",
+      location: [50.99922255022839, 7.1245154852040455],
+      _location: [50.9914574, 7.096766],
+    },
+    825: {
+      //Finja home
+      code: 13,
+      hint: "Hinter Gittern! Haut als Team soviel Bier weg wie es streben auf Finjas Toilette (von außen) gibt!",
+      location: [50.9958535, 7.1011889],
+    },
+  },
+
+  3: {
+    88416: {
+      //spielplatz
+      code: 825,
+      hint: "Jetzt steht ein kleiner Parkourlauf an! Schnappt euch ein Bier, rutscht die Rutsche runter, lauft geradeaus auf das Kletter-/Seilgerüst zu und klettert einmal im Kreis. Dabei muss bis zum Ende ein Bier getrunken werden! (Bitte filmen und in die Gruppe schicken; Bier muss im Video geöffnet werden und am Ende nachgewiesen leer sein/vom Läufer getrunken)",
+      location: [50.9933246, 7.1046091],
+    },
+    825: {
+      //kita
+      code: 216,
+      hint: "Ob ihr zählen und rechnen könnt? Wieviele Buchstaben sind auf dem Klingelschild der Kindertagesstätte, multipliziert mit dem morgigen Alter der Gastgeberin?",
+      location: [50.9971046, 7.1007708],
+    },
+    0: {
+      //hansen
+      code: 492,
+      hint: "Wat fott es, es fott! Kauft ein Kölsch im Wirtshaus Hansen, ext es und der Barkeeper sagt euch die nächste Nummer!",
+      location: [50.998862, 7.0999595],
+    },
+    492: {
+      //hochhäuser
+      code: 88416,
+      hint: "Hoch hinaus! Die Hochhäuser haben einen kleinen Park mit 9 Bäumen, addiert die Nummern die auf ihnen stehen, das ergibt die nächste Zahl!",
+      location: [50.99922255022839, 7.1245154852040455],
+      _location: [50.9914574, 7.096766],
+    },
+    216: {
+      //Finja home
+      code: 13,
+      hint: "Hinter Gittern! Haut als Team soviel Bier weg wie es streben auf Finjas Toilette (von außen) gibt!",
+      location: [50.9958535, 7.1011889],
+    },
+  },
+
+  4: {
+    88416: {
+      //spielplatz
+      code: 825,
+      hint: "Jetzt steht ein kleiner Parkourlauf an! Schnappt euch ein Bier, rutscht die Rutsche runter, lauft geradeaus auf das Kletter-/Seilgerüst zu und klettert einmal im Kreis. Dabei muss bis zum Ende ein Bier getrunken werden! (Bitte filmen und in die Gruppe schicken; Bier muss im Video geöffnet werden und am Ende nachgewiesen leer sein/vom Läufer getrunken)",
+      location: [50.9933246, 7.1046091],
+    },
+    825: {
+      //kita
+      code: 216,
+      hint: "Ob ihr zählen und rechnen könnt? Wieviele Buchstaben sind auf dem Klingelschild der Kindertagesstätte, multipliziert mit dem morgigen Alter der Gastgeberin?",
+      location: [50.9971046, 7.1007708],
+    },
+    216: {
+      //hansen
+      code: 492,
+      hint: "Wat fott es, es fott! Kauft ein Kölsch im Wirtshaus Hansen, ext es und der Barkeeper sagt euch die nächste Nummer!",
+      location: [50.998862, 7.0999595],
+    },
+    0: {
+      //hochhäuser
+      code: 88416,
+      hint: "Hoch hinaus! Die Hochhäuser haben einen kleinen Park mit 9 Bäumen, addiert die Nummern die auf ihnen stehen, das ergibt die nächste Zahl!",
+      location: [50.99922255022839, 7.1245154852040455],
+      _location: [50.9914574, 7.096766],
+    },
+    492: {
+      //Finja home
+      code: 13,
+      hint: "Hinter Gittern! Haut als Team soviel Bier weg wie es streben auf Finjas Toilette (von außen) gibt!",
+      location: [50.9958535, 7.1011889],
+    },
+  },
+};
+
+const updateDistance = 50;
+
+const getDegree = (A, B) => {
+  let v =
+    Math.acos(
+      (B[1] - A[1]) /
+        Math.sqrt(Math.pow(B[0] - A[0], 2) + Math.pow(B[1] - A[1], 2))
+    ) *
+    (180 / Math.PI);
+
+  if (A[0] > B[0]) v += (180 - v) * 2;
+
+  return v;
+};
+
+const getTargetStatus = async (B) => {
+  let v = await new Promise((r, e) =>
+    navigator.geolocation.getCurrentPosition(r, r, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+    })
+  );
+  if (v.code) alert("Error: " + JSON.stringify(v));
+
+  let distance =
+    calcCrow(v.coords.latitude, v.coords.longitude, B[0], B[1]) * 1000;
+
+  let A = [v.coords.longitude, v.coords.latitude];
+  return { degree: getDegree(A, [B[1], B[0]]), distance };
+};
+
+const calcCrow = (lat1, lon1, lat2, lon2) => {
+  let R = 6371;
+  let dLat = toRad(lat2 - lat1);
+  let dLon = toRad(lon2 - lon1);
+  lat1 = toRad(lat1);
+  lat2 = toRad(lat2);
+
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  return d;
+};
+
+const toRad = (Value) => {
+  return (Value * Math.PI) / 180;
+};
+
+console.log(await getTargetStatus([51.00000566999777, 7.1325991073443875]));
+
+const route = routes[localStorage.GCODE];
+
+const direction = document.getElementById("direction");
+const distance = document.getElementById("distance");
+const instructions = document.getElementById("instructions");
+const code = document.getElementById("code");
+const send = document.getElementById("send");
+
+send.addEventListener("click", () => {
+  if (!route[code.value]) {
+    alert("falsch");
+    return (code.value = "");
+  }
+  alert("Richtig");
+  localStorage.code = code.value;
+});
+
+const updateData = async () => {
+  if (!localStorage.code) localStorage.code = 0;
+  let station = route[localStorage.code];
+  let status = await getTargetStatus(station.location);
+  direction.innerText = Math.floor(status.degree) + "°";
+  distance.innerText = Math.floor(status.distance) + "M";
+  instructions.innerText = status.distance < updateDistance ? station.hint : "";
+};
+
+let updateLoop = async () => {
+  await updateData();
+  setTimeout(() => updateLoop(), 500);
+};
+
+updateLoop();
+
+//lat = y;
+//long = x;
+
+/*console.log(
+  getDegree(
+    [7.124540780424274, 50.99938828690538],
+    [7.132565949250805, 51.00001621532612]
+  )
+);*/
